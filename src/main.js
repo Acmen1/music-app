1043404d21848d15212bcd663b688e8d8277de86
Acmen <18438652759@163.com>
2018-10-20 09:28:15 +0800

    版本1
    第一次提交

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+
+import 'common/stylus/index.styl'
+
+Vue.config.productionTip = false
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+fastclick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
